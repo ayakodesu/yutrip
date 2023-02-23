5a5770478cabef2c6062b785c765c41fcbf147c9
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-02-23 08:13:44 +0000

    [Add]adminサイン出来るようにした

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -23,5 +23,14 @@ class User::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+
+
+  def after_sign_in_path_for(resource)
+    user_homes_top_path
+  end
+
+  def after_sign_out_path_for(resource)
+    user_homes_about_path
+  end
   # end
 end
