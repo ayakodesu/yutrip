c8a65e482aa1847f31b71bc1bc503738d952a9a1
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-02-23 02:09:47 +0000

    [Add]routesのdevise_for:adminと:userをコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,22 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
-  devise_for :admins
-  devise_for :users
+  #devise_for :admins
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 # 顧客用
 # URL /users/sign_in ...
-#devise_for :users,skip: [:passwords], controllers: {
-  #registrations: "user/registrations",
-  #sessions: 'user/sessions'
-#}
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
 
 # 管理者用
 # URL /admin/sign_in ...
-#devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  #sessions: "admin/sessions"
-#}
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 
 
 
