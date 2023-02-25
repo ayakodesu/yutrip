5c3fcf62d76b062090975d840bd8c1ef19f8af5f
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-02-25 11:21:55 +0000

    [Add]ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :user do
-    get 'homes/top'
-    get 'homes/about'
-    get 'favorites/index'
-    get 'favorites/destroy'
-    get 'favorites/create'
-    get 'spots/new'
-    get 'spots/index'
-    get 'spots/edit'
-    get 'spots/destroy'
-    get 'spots/show'
-    get 'spots/update'
-    get 'spots/create'
+    get '/' => 'homes#top'
+    get "/home/about" => "homes#about", as: "about"
+    resource :favorites, only: [index, destroy, create]
+    resources :spots, only: [new, index, edit, destroy ,show, update, create]
   end
 
   namespace :admin do
-    get 'spots/new'
-    get 'spots/index'
-    get 'spots/edit'
-    get 'spots/destroy'
-    get 'spots/show'
-    get 'spots/update'
-    get 'spots/create'
-    get 'categories/new'
-    get 'categories/index'
-    get 'categories/edit'
-    get 'categories/destroy'
     get 'homes/top'
+    resources :spots, only: [new, index, edit, destroy, show, update, create]
+    resources :categories, only: [new, index, edit, destroy]
   end
 
   #devise_for :admins
