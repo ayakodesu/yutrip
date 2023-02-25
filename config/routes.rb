eabd88c2eee16c06b204878dc3ebc22da72534fa
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-02-25 11:14:26 +0000

    [Add]ルーティング不要箇所削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   namespace :user do
+    get 'homes/top'
+    get 'homes/about'
     get 'favorites/index'
     get 'favorites/destroy'
     get 'favorites/create'
-  end
-  namespace :user do
     get 'spots/new'
     get 'spots/index'
     get 'spots/edit'
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
     get 'spots/update'
     get 'spots/create'
   end
+
   namespace :admin do
     get 'spots/new'
     get 'spots/index'
@@ -21,20 +23,13 @@ Rails.application.routes.draw do
     get 'spots/show'
     get 'spots/update'
     get 'spots/create'
-  end
-  namespace :admin do
     get 'categories/new'
     get 'categories/index'
     get 'categories/edit'
     get 'categories/destroy'
-  end
-  namespace :admin do
     get 'homes/top'
   end
-  namespace :user do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
   #devise_for :admins
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
