4a7906b693f9dc12318dd0b7cf53220455f7cab9
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-02-28 13:13:50 +0000

    [Add]ブートストラップ使えるように設定

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -26,7 +26,7 @@ class User::SessionsController < Devise::SessionsController
 
 
   def after_sign_in_path_for(resource)
-    user_homes_about_path
+    user_path
   end
 
   def after_sign_out_path_for(resource)
