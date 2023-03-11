6a3da4f9d28f48387c5f5495a90611616b3e955f
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-11 11:54:17 +0000

    [Add]user投稿できるようにした

--- app/controllers/user/spots_controller.rb
+++ app/controllers/user/spots_controller.rb
@@ -1,5 +1,15 @@
 class User::SpotsController < ApplicationController
   def new
+    @spot = Spot.new
+  end
+
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @spot = Spot.new(spot_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @spot.save
+    # 4. spotsのindexページへリダイレクト
+    redirect_to user_spots_path
   end
 
   def index
