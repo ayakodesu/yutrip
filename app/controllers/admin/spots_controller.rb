f8f674bb2d41dd19c9269dbd78f9447b1cf16f69
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-12 03:27:50 +0000

    [Add]adminサイドメニュー追加

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -3,6 +3,15 @@ class Admin::SpotsController < ApplicationController
     @spot = Spot.new  # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
   end
 
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @spot = Spot.new(spot_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @spot.save
+    # 4. spotsのindexページへリダイレクト
+    redirect_to admin_spots_path
+  end
+
   def index
     @spots = Spot.all
   end
@@ -19,14 +28,7 @@ class Admin::SpotsController < ApplicationController
   def update
   end
 
-  def create
-     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    @spot = Spot.new(spot_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    @spot.save
-    # 4. spotsのindexページへリダイレクト
-    redirect_to admin_spots_path
-  end
+
 
   private
 
