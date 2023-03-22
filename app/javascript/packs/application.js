59843e853a44cdf3dd4b39b2f897c6bd30e15f02
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-22 13:48:01 +0000

    [Add]topのスライドショー変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,11 +23,58 @@ import "../stylesheets/application"
 
 
 $(function () {
-  $(".slider").slick({
-    arrows: false,
-    autoplay: true,
-    adaptiveHeight: true,
-    dots: true,
+  var $slider4 = $('#js-slider-4');
+
+  $slider4.slick({
+    arrows: false, // 前・次のボタンを表示しない
+    dots: true, // ドットナビゲーションを表示する
+    appendDots: $('.dots-4'), // ドットナビゲーションの生成位置を変更
+    fade: true, // スライド切り替えをフェード
+    autoplay: false, //自動再生させない
+    slidesToShow: 1, // 表示させるスライド数
+  });
+
+  /*--- プログレスバー設定 -----------------------*/
+  var
+    time = 3,
+    $bar = $('#js-progressBar'),
+    isPause,
+    tick,
+    percentTime;
+  function startProgressbar() {
+    resetProgressbar();
+    percentTime = 0;
+    isPause = false;
+    tick = setInterval(interval, 10);
+  }
+  function interval() {
+    if (isPause === false) {
+      percentTime += 1 / (time + 0.1);
+      $bar.css({
+        width: percentTime + "%"
+      });
+      if (percentTime >= 100) {
+        $slider4.slick('slickNext');
+        startProgressbar();
+      }
+    }
+  }
+  function resetProgressbar() {
+    $bar.css({
+      width: 0 + '%'
+    });
+    clearTimeout(tick);
+  }
+  startProgressbar();
+
+  // カーソルが乗ったら止める
+  $slider4.on({
+    mouseenter: function () { isPause = true; },
+    mouseleave: function () { isPause = false; }
+  });
+
+  // ドットがクリックされたら再発火(スライド切り替え前のイベント)
+  $slider4.on('beforeChange', function (slick, currentSlide, nextSlide) {
+    startProgressbar();
   });
 });
-
