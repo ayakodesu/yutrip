4cb2eccb34e4b7d2db3c37d86282b5f50cc0ac85
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-26 08:30:06 +0000

    [Add]FontAwesome導入

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,9 +3,12 @@ class Spot < ApplicationRecord
 has_one_attached :image
 
 has_many :reviews, dependent: :destroy
-
+has_many :favorites, dependent: :destroy
 belongs_to :category, optional: true
 
+def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+end
 
 
 end
