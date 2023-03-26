4e6548131944229265bfca122a64c976b4cd04df
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-26 08:40:23 +0000

    [Add]お気に入り一覧ヘッダーに付けた

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   namespace :user do
     get '/' => 'homes#top'
     get 'homes/about'
-    resource :favorites, only: [:destroy, :create]
+    resources :favorites, only: [:destroy, :create, :index]
     resources :spots, only: [:new, :index, :edit, :destroy, :show, :update, :create]
     resources :reviews, only: [:new, :index, :edit, :destroy, :show, :update, :create]
     resources :favorites, only: [:index]
+
   end
 
   namespace :admin do
