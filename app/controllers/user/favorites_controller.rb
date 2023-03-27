2709bb50eb3598f397b1d1f27506852c402832ee
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-27 13:20:35 +0000

    [Add]userお気に入り機能コントローラー追加

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,5 +1,6 @@
 class User::FavoritesController < ApplicationController
   def index
+    @favorite = current_user.favorites
   end
 
   def destroy
@@ -7,4 +8,8 @@ class User::FavoritesController < ApplicationController
 
   def create
   end
+  private
+    def favorite_params
+      params.require(:favorite).permit(:category_id, :spot_id, :name, :quality, :introduction, :image)
+    end
 end
