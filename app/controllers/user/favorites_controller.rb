510146538edf565064a0f5fed301cec3ef250301
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-27 13:58:48 +0000

    [Add]お気に入り機能修正

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -7,14 +7,14 @@ class User::FavoritesController < ApplicationController
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.find_by(spot_id: spot.id)
     @favorite.destroy
-    redirect_to spot_path(spot)
+    redirect_to user_spots_path
   end
 
   def create
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.new(spot_id: spot.id)
     @favorite.save
-    redirect_to spot_path(spot)
+    redirect_to user_spots_path
   end
 
   private
