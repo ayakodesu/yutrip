7f6ea8d0c4cca9430daa9f0184db4a372d2b60b8
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-03-28 01:28:07 +0000

    [Add]favoriteモデル訂正

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,20 +1,19 @@
 class User::FavoritesController < ApplicationController
+
+
   def index
-    @favorite = current_user.favorites
+    @favorites = current_user.favorites
   end
 
   def destroy
     @spot = Spot.find(params[:spot_id])
-    @favorite = current_user.favorites.find_by(spot_id: spot.id)
+    @favorite = current_user.favorites.find_by(spot_id: @spot.id)
     @favorite.destroy
     redirect_to user_spots_path
   end
 
   def create
-    @spot = Spot.find(params[:spot_id])
-    @favorite = current_user.favorites.new(spot_id: spot.id)
-    @favorite.save
-    redirect_to user_spots_path
+    @favorite = current_user.favorites.create(spot_id: params[:spot_id])
   end
 
   private
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,5 +3,5 @@ class Favorite < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user, optional: true
-  belongs_to :spot, optional: true
+  belongs_to :spot
 end
