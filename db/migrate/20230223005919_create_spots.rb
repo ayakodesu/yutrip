51a775603f2df797e428ab7ad1a90c7adca952ed
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-04-05 14:18:25 +0000

    [Add]migrate　spots修正

--- db/migrate/20230223005919_create_spots.rb
+++ db/migrate/20230223005919_create_spots.rb
@@ -3,8 +3,8 @@ class CreateSpots < ActiveRecord::Migration[6.1]
     create_table :spots do |t|
       t.string :name, null: false
       t.text :introduction
-      t.integer :quality, null: false
-      t.integer :characteristics, null: false
+      t.string :quality, null: false
+      t.boolean :characteristics, null: false, default: true
       t.integer :praice
       t.integer :category_id, null: false
       t.boolean :is_active, null: false, default: true
