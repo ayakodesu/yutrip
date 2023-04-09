80c6730ee9715c6d8d0d6c3decc9775f463ecfb9
ayakodesu <109893338+ayakodesu@users.noreply.github.com>
2023-04-09 00:20:06 +0000

    [Add]reviewのmodel設定

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -3,6 +3,11 @@ class User::ReviewsController < ApplicationController
   end
 
   def create
+    @spot = Spot.find(params[:spot_id])
+    comment = current_user.spots.new(review_params)
+    comment.spot_id = spot.id
+    comment.save
+    redirect_to user_spots_path(spot)
   end
 
   def index
@@ -12,5 +17,11 @@ class User::ReviewsController < ApplicationController
   end
 
   def edit
+  end
+
+   private
+
+  def review_params
+    params.require(:review).permit(:comment)
   end
 end
